7ad4d7924afd08dc7351f3f04cd6b89ce9933f37
nikorient <denisovnikolay04@gmail.com>
2023-03-05 15:24:34 +0300

    addition

---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+
+            Console.Write("Введите А ");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите В ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            C = A + B;
+            Console.Write($"Ответ сложения: {C} ");
+        }
+    }
+}
